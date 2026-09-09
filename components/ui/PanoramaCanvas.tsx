c6c13457774cbf7dc8a1a31381e542f7f1db081c
ChangShuKai <csk1681311@gmail.com>
2026-09-09 18:36:34 +0800

    perf: optimize LCP image loading in PanoramaCanvas

--- components/ui/PanoramaCanvas.tsx
+++ components/ui/PanoramaCanvas.tsx
@@ -118,6 +118,8 @@ export default function PanoramaCanvas() {
           pointerEvents: "none",
         }}
         draggable={false}
+        fetchPriority="high"
+        decoding="async"
       />
     </div>
   );
